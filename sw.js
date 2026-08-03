da370d430804cd9b2d947b10830e9285d31c5def
nijonew <nicholasjnewbold@gmail.com>
2026-08-03 12:29:30 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // It only lets the app's own shell (HTML/CSS/JS) load instantly and
 // work even with a flaky connection.
 
-const CACHE_NAME = "nutrition-app-shell-v30";
+const CACHE_NAME = "nutrition-app-shell-v31";
 const SHELL_FILES = [
   "./index.html",
   "./log.html",
